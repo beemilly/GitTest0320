41ffc91dca9a16d9494a6ac0222f80492ab5be9b
beemilly <beemillyjp@gmail.com>
2021-03-20 13:32:50 +0900

    first commit

---
+++ src/test/java/com/kr/study/test/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.kr.study.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testSum() {
+
+		Calculator calculator = new Calculator();
+
+		assertEquals(30, calculator.sum(10, 10));
+
+	}
+
+}
